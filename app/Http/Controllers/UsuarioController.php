c33522a2e0892155e548b6c360f71accc2d35a83
Marco <marco1102004@gmail.com>
2024-02-16 12:55:34 -0600

    cambios

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,18 +7,15 @@ use Illuminate\Http\Request;
 class UsuarioController extends Controller
 {
     public function registro(Request $request){
-        $data = $request->all();
-        
-        response()->json([
-            'data' => $data,
-            'message' => 'Registro exitoso'
+        return response()->json([
+            'message' => 'Registro de usuario'
         ]);
     }
 }
 
 
 // pull a server 
-// sudo chown -R www-data:www-data /var/www/html
-// git pull
 // sudo chown -R ubuntu:ubuntu /var/www/html
+// git pull
+// sudo chown -R www-data:www-data /var/www/html
 // sudo systemctl restart apache2
