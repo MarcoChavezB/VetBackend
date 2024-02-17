a1a5548fc5d402ff388497142e861f71a75e3999
Marco <marco1102004@gmail.com>
2024-02-17 12:35:06 -0600

    endpoint producos por nombre

--- app/Http/Controllers/MostrarProductosController.php
+++ app/Http/Controllers/MostrarProductosController.php
@@ -57,6 +57,23 @@ class MostrarProductosController extends Controller
 
     public function getProductoPublicoByName($name){
         $resultados = DB::select("CALL producto_venta_nombre(?)", [$name]);
+        
+        if(empty($name)){
+            $resultado = DB::table('productos')
+            ->select('id', 'nom_producto', 'descripcion', 'tipo_producto', 'imagen')
+            ->selectRaw('MAX(existencias) as existencias')
+            ->selectRaw('MAX(precio_venta) as precio_venta')
+            ->selectRaw('(MAX(precio_venta) * 0.16) as iva')
+            ->selectRaw("CASE WHEN MAX(existencias) <= 0 THEN 'Sin stock' ELSE 'Stock' END as estado")
+            ->where('tipo_producto', 'venta')
+            ->groupBy('id', 'nom_producto', 'descripcion', 'tipo_producto', 'imagen')
+            ->get();
+    
+            return response()->json([
+                'productos' => $resultado
+            ]);
+        }
+
         return response()->json([
             'productos' => $resultados
         ]);
