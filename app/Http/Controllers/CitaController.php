b074fb03f67a0347ca1c6f19d542f68ccfbb4fab
Marco <marco1102004@gmail.com>
2024-02-16 22:24:48 -0600

    añadiendo endpoint citas hoy

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -10,7 +10,13 @@ class CitaController extends Controller
     public function getCitasProximas(){
         $citas = Cita::whereBetween('fecha_cita', [now(), now()->addDays(2)])
         ->get();
+        return response()->json([
+            'citas' => $citas
+        ]);
+    }
 
+    public function citasTotalHoy(){
+        $citas = Cita::whereDate('fecha_cita', now())->get();
         return response()->json([
             'citas' => $citas
         ]);
